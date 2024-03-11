893c6f8c3ddc294cf59b777d3b3bcb851509e03b
EJRP123 <e1j2r3p4@gmail.com>
2024-03-10 20:12:15 -0400

    Added an option for adding audio to the video

--- cli/src/main/kotlin/com/ejrp/Main.kt
+++ cli/src/main/kotlin/com/ejrp/Main.kt
@@ -6,6 +6,7 @@ import com.ejrp.midi.*
 import com.ejrp.video.Dimensions
 import com.ejrp.video.Frame
 import com.ejrp.video.HumbleVideo
+import com.ejrp.video.HumbleVideoWithAudio
 import io.humble.video.*
 import java.awt.image.BufferedImage
 import java.io.File
@@ -17,30 +18,40 @@ If you want to test this project out, run these commands:
 cd cli
 mvn package -P linux # Or win64 if you are on windows
 cd target
-java -jar MidiToImgVid-cli-1.0-jar-with-dependencies.jar <midi file path> <images folder path> <format name> <width> <height>
+java -jar MidiToImgVid-cli-1.0-jar-with-dependencies.jar <midi file path> <audio file path> <images folder path> <format name> <width> <height>
  */
 
 fun main(args: Array<String>) {
     val startTime = System.currentTimeMillis()
-    require(args.size == 5) { arguments }
+    require(args.size == 6) { arguments }
     // Harcoding it cause Maven moves the lib files anyway
     System.setProperty("java.library.path","./lib")
+
     val midiPath = args[0]
-    val imagePath = args[1]
-    val formatName = args[2]
-    val width = args[3].toInt()
-    val height = args[4].toInt()
     require(isValidFilePath(midiPath))
+
+    val audioPath = args[1]
+    val isAudioEncoded = audioPath != "-"
+    if (isAudioEncoded) require(isValidFilePath(audioPath))
+
+    val imagePath = args[2]
     require(isValidFolderPath(imagePath))
+
+    val formatName = args[3]
     require(MuxerFormat.getFormats().map { it.name }.contains(formatName)) {
         "The format inputted $formatName is not valid!"
     }
+
+    val width = args[4].toInt()
+    val height = args[5].toInt()
+
     val outputPath = "./output.$formatName"
     createFile(outputPath) // Just to make sure
     println("Creating video from midi file \"$midiPath\"\n" +
-            "using images from folder $imagePath\n" +
-            "with the format $formatName\n" +
-            "with a width of $width and height of $height")
+            "\twith audio from file \"$audioPath\"" +
+            "\tusing images from folder $imagePath\n" +
+            "\twith the format $formatName\n" +
+            "\twith a width of $width and height of $height")
 
     val midiFile = FileInputStream(midiPath).parseMidiFile()
     // Only dealing with format 0 or 1 tracks
@@ -60,7 +71,6 @@ fun main(args: Array<String>) {
     val frameTimeLength = System.currentTimeMillis()
     print("Setting the length of each image in the video")
     val ticksOfNoteOnFromStart = getNotesTicksFromStart(midiFile)
-
     val images = File(imagePath).listFiles()!!
         .filter { file ->
             try {
@@ -71,7 +81,8 @@ fun main(args: Array<String>) {
             }
         }
         .map { ImageIO.read(it).resize(width, height).convertToType(BufferedImage.TYPE_3BYTE_BGR) }
-        .map { it.resize(width, height).convertToType(BufferedImage.TYPE_3BYTE_BGR) }
+        .shuffled() // Added the shuffling of the list. Will maybe add it as a flag (-s)
+
     require(images.isNotEmpty()) { "There is no images that the ImageIO class can load. Note that the ImageIO class supports " +
             "by default these image file types: bpm, gif, jpeg, png, tiff and wbpm" }
     var imageIndex = -1
@@ -86,18 +97,29 @@ fun main(args: Array<String>) {
 
     val encodingVideoTime = System.currentTimeMillis()
     print("Encoding the images into a video file")
-    val humbleVideo = HumbleVideo(
-        Dimensions(width, height),
-        timebaseInSecondPerMidiTicks,
-        formatName,
-        frames
-    )
-    humbleVideo.writeToFile(outputPath)
+
+    if (isAudioEncoded) {
+        HumbleVideoWithAudio(
+            Dimensions(width, height),
+            timebaseInSecondPerMidiTicks,
+            formatName,
+            frames,
+            audioPath
+        ).writeToFile(outputPath)
+    } else {
+        HumbleVideo(
+            Dimensions(width, height),
+            timebaseInSecondPerMidiTicks,
+            formatName,
+            frames
+        ).writeToFile(outputPath)
+    }
+
     print(" (${(System.currentTimeMillis() - encodingVideoTime) / 1000} seconds)\n")
     println("DONE (${(System.currentTimeMillis() - startTime) / 1000} seconds)")
 }
 
-const val arguments = "Arguments: <midi file path> <images folder path> <format name> <width> <height>"
+const val arguments = "Arguments: <midi file path> <audio file path (put `-` if no audio)> <images folder path> <format name> <width> <height>"
 
 /**
  * Is the file path provided valid
